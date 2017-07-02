fcae6fcb5133ae01adec577333175ceacf2c7ab6
iwan roberts <ilroberts@gmail.com>
2017-07-02 15:10:48 +0100

    moment installed and working

---
+++ app/helpers/formatDate.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+import moment from 'moment';
+
+export function formatDate(params){
+
+	return moment(params[0]).format('YYYY-MM-DD');
+}
+
+export default Ember.Helper.helper(formatDate);
